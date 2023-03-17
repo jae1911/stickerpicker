4e157b6a66fe02bfa55d29320a8fc2f5619a1daf
Jae Lo Presti (DN0) <git@777.tf>
2023-03-17 10:42:17 +0200

    add stickers

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '6be541e0'
+version = '0.1.0+dev.6be541e0'
+linkified_version = '0.1.0+dev.[6be541e0](https://github.com/maunium/stickerpicker/commit/6be541e0f8765cde97339b2feb5a92c1498ea436)'
