0a1d031b5833f6b4a1b191019901a93bcb96c98a
Jae Lo Presti [j4] <git@777.tf>
2024-02-25 14:40:03 +0200

    meta: remove packs

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '6be541e0'
-version = '0.1.0+dev.6be541e0'
-linkified_version = '0.1.0+dev.[6be541e0](https://github.com/maunium/stickerpicker/commit/6be541e0f8765cde97339b2feb5a92c1498ea436)'
+git_revision = '9d77f44b'
+version = '0.1.0+dev.9d77f44b'
+linkified_version = '0.1.0+dev.[9d77f44b](https://github.com/maunium/stickerpicker/commit/9d77f44b3f34b3488faeb47432f3f5a9d78dd3c3)'
